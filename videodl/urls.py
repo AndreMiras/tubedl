65bf75873213984164169f54a3e1ee838361c16d
Andre Miras <andre.miras@gmail.com>
2016-05-24 23:35:28 +0200

    PEP8 fixes WIP E128

--- videodl/urls.py
+++ videodl/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('videodl.views',
+urlpatterns = patterns(
+    'videodl.views',
     # url(r'^$', 'download_form', name='download_form'),
     url(r'^download_form/$', 'download_form', name='download_form'),
     url(r'^video_info/(?P<download_link_uuid>[\w]+)/$',
