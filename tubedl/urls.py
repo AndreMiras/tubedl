2a4ba00a0442e481e868a10a15f3a14d6ce3c431
Andre Miras <andre.miras@gmail.com>
2016-05-24 23:59:26 +0200

    PEP8 E128 fix

--- tubedl/urls.py
+++ tubedl/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 handler500 = 'tubedl.views.custom_500'
 
-urlpatterns = patterns('',
+
+urlpatterns = patterns(
+    '',
     url(r'^$', 'tubedl.views.home', name='home'),
     url(r'^contact/$', 'tubedl.views.contact', name='contact'),
     url(r'^error404/$', 'tubedl.views.error404', name='error404'),
