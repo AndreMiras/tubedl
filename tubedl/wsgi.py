ffc076b5f9bf19b75c19b9aba8750d3e77b29941
Andre Miras <andre.miras@gmail.com>
2016-05-24 23:49:49 +0200

    Ignore PEP8 E402 errors in wsgi

--- tubedl/wsgi.py
+++ tubedl/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tubedl.settings")
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from django.core.wsgi import get_wsgi_application  # noqa
+from dj_static import Cling  # noqa
 
 application = Cling(get_wsgi_application())
