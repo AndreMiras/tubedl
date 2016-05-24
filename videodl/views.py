AndreMiras/tubedl

commit 186702262db3c76520ca0b59229257ba1992af1d

PEP8 fixes WIP E128

videodl/views.py
@@ -192,16 +192,18 @@ def prepare_download_redirect(request, download_link_uuid):
                 # raises the exception so the admins get notified
                 raise
             if audio_only:
-                download_redirect_url = reverse('serve_audio_download',
-                    kwargs={'download_link_uuid': download_link_uuid})
+                redirect_url = reverse(
+                        'serve_audio_download',
+                        kwargs={'download_link_uuid': download_link_uuid})
             else:
-                download_redirect_url = reverse('serve_video_download',
-                    kwargs={'download_link_uuid': download_link_uuid})
-            data = {"download_redirect_url": download_redirect_url}
-            # return HttpResponseRedirect(download_redirect_url)
+                redirect_url = reverse(
+                        'serve_video_download',
+                        kwargs={'download_link_uuid': download_link_uuid})
+            data = {"redirect_url": redirect_url}
             return JsonResponse(data)
     return HttpResponseRedirect(reverse('video_info',
-        kwargs={'download_link_uuid': download_link_uuid}))
+                                kwargs={
+                                    'download_link_uuid': download_link_uuid}))
 
 
 def serve_download_helper(request, download_link_uuid, extract_audio=False):

commit 26ed2063211c94f66c8d13937fd10ca8f3f6d6e8

Fixes regression
Introduced in 186702262db3c76520ca0b59229257ba1992af1d
download_redirect_url is expected by video_info.js

videodl/views.py
@@ -197,14 +197,14 @@ def prepare_download_redirect(request, download_link_uuid):
                 # raises the exception so the admins get notified
                 raise
             if audio_only:
-                redirect_url = reverse(
+                download_redirect_url = reverse(
                         'serve_audio_download',
                         kwargs={'download_link_uuid': download_link_uuid})
             else:
-                redirect_url = reverse(
+                download_redirect_url = reverse(
                         'serve_video_download',
                         kwargs={'download_link_uuid': download_link_uuid})
-            data = {"redirect_url": redirect_url}
+            data = {"download_redirect_url": download_redirect_url}
             return JsonResponse(data)
     return HttpResponseRedirect(reverse('video_info',
                                 kwargs={