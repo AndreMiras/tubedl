064eb819a5962796aa3307cc27ad69c79dc1fd25
Andre Miras <andre.miras@gmail.com>
2016-05-25 00:00:35 +0200

    PEP8 E222 E225 fixes

--- tubedl/prod_settings.py
+++ tubedl/prod_settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 if os.environ.get('ADMIN_NAME') and os.environ.get('ADMIN_EMAIL'):
     ADMINS = (
@@ -16,7 +16,7 @@ if os.environ.get('ADMIN_NAME') and os.environ.get('ADMIN_EMAIL'):
     MANAGERS = ADMINS
 
 EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME', '')
-EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD', '')
