1358aa090ee8f5183ac66ba12ed3639a6c9bb8ef
Andre Miras <andre.miras@gmail.com>
2018-10-06 18:39:49 +0200

    Integrates to Sentry, fixes #7
    Settings has been updated on Heroku using:
    ```
    heroku config:set
    ```

--- tubedl/prod_settings.py
+++ tubedl/prod_settings.py
@@ -1,4 +1,6 @@
 import dj_database_url
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 from .base_settings import *  # noqa
 
@@ -21,3 +23,11 @@ EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD', '')
+
+# configure Sentry
+SENTRY_PUBLIC_KEY = os.environ.get('SENTRY_PUBLIC_KEY')
+SENTRY_PROJECT_ID = os.environ.get('SENTRY_PROJECT_ID')
+
+if None not in [SENTRY_PUBLIC_KEY, SENTRY_PROJECT_ID]:
+    dsn = f'https://{SENTRY_PUBLIC_KEY}@sentry.io/{SENTRY_PROJECT_ID}'
+    sentry_sdk.init(dsn=dsn, integrations=[DjangoIntegration()])
