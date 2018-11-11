70e68f80c90b7b63e04334d19175946b9fcb1884
Andre Miras <andre.miras@gmail.com>
2018-11-11 22:09:00 +0100

    Fixes keepalive script
    Error was:
    ```
    No module named 'urllib2'
    ```

--- tubedl/management/commands/keepalive.py
+++ tubedl/management/commands/keepalive.py
@@ -1,10 +1,14 @@
 import os
-import urllib2
+import requests
 
 from django.core.management.base import NoArgsCommand
 
 
 class Command(NoArgsCommand):
+    """
+    Usage example:
+    HOSTNAME=tubedl.herokuapp.com python manage.py keepalive
+    """
     help = 'Pings os.environ["HOSTNAME"] to keep dyno alive.'
 
     def handle_noargs(self, **options):
@@ -12,6 +16,6 @@ class Command(NoArgsCommand):
         if hostname:
             self.stdout.write('ping "%s"' % hostname)
             url = "http://" + hostname
-            urllib2.urlopen(url)
+            requests.get(url)
         else:
             self.stdout.write("Couldn't keep alive, HOSTNAME not set")
