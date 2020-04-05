17a3d43e7b7d3ca31d61b97a5364f058aac16b8a
Andre Miras <andre.miras@gmail.com>
2020-04-05 15:21:21 +0200

    patterns() is deprecated
    https://docs.djangoproject.com/en/1.10/releases/1.8/#deprecated-features-1-8

--- videodl/urls.py
+++ videodl/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.contrib import admin
+
+from videodl import views
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    'videodl.views',
-    # url(r'^$', 'download_form', name='download_form'),
-    url(r'^download_form/$', 'download_form', name='download_form'),
+urlpatterns = [
+    url(r'^download_form/$', views.download_form, name='download_form'),
     url(r'^video_info/(?P<download_link_uuid>[\w]+)/$',
-        'video_info', name='video_info'),
+        views.video_info, name='video_info'),
     url(r'^prepare_download_redirect/(?P<download_link_uuid>[\w]+)/$',
-        'prepare_download_redirect', name='prepare_download_redirect'),
+        views.prepare_download_redirect, name='prepare_download_redirect'),
     url(r'^serve_video_download/(?P<download_link_uuid>[\w]+)/$',
-        'serve_video_download', name='serve_video_download'),
+        views.serve_video_download, name='serve_video_download'),
     url(r'^serve_audio_download/(?P<download_link_uuid>[\w]+)/$',
-        'serve_audio_download', name='serve_audio_download'),
-    url(r'^supported_sites/$', 'supported_sites', name='supported_sites'),
-)
+        views.serve_audio_download, name='serve_audio_download'),
+    url(r'^supported_sites/$', views.supported_sites, name='supported_sites'),
+]
